6b4e18b6e260c6213651f499755a4360ca45a57d
冷月 <lifei.zhong@icloud.com>
2025-05-10 00:02:59 +0800

    Initial Commit

---
+++ AdventureRecords/AdventureRecordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureRecordsApp.swift
+//  AdventureRecords
+//
+//  Created by Lengyue's Macbook on 2025/5/10.
+//
+
+import SwiftUI
+
+@main
+struct AdventureRecordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventureRecordsTests/AdventureRecordsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureRecordsTests.swift
+//  AdventureRecordsTests
+//
+//  Created by Lengyue's Macbook on 2025/5/10.
+//
+
+import Testing
+@testable import AdventureRecords
+
+struct AdventureRecordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
